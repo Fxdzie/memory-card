1600d3375ffa4f0fe4dc37e5bcc4aac07ef98132
Fxdzie <manwafadziso@gmail.com>
2025-05-11 23:56:17 +0200

    Addition of styling and functionality of card subtitle

---
+++ src/components/Subtitle/Subtitle.jsx
@@ -0,0 +1,11 @@
+import './Subtitle.css'
+
+function Subtitle() {
+    return (
+      <h2 className="subtitle-container">
+        Get points by clicking on a pokemon but don't click on any more than once!
+      </h2>
+    );
+  }
+  
+  export default Subtitle;
